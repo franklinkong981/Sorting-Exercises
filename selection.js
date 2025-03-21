839caeadac8e2be005de5b34d5b98acb61423561
Franklin Kong <franklinkong981@gmail.com>
2025-03-21 13:16:53 -0700

    Successfully implemented selection sort

--- selection.js
+++ selection.js
@@ -1,3 +1,20 @@
-function selectionSort() {}
+function selectionSort(arr) {
+  for (let i = 0; i < arr.length - 1; i++) {
+    let minimumIndex = i;
+    for (let j = i + 1; j < arr.length; j++) {
+      if (arr[j] < arr[minimumIndex]) {
+        minimumIndex = j;
+      }
+    }
+
+    if (minimumIndex != i) {
+      let temp = arr[i];
+      arr[i] = arr[minimumIndex];
+      arr[minimumIndex] = temp;
+    }
+  }
+
+  return arr;
+}
 
 module.exports = selectionSort;
