479bdc6d2b812239b3772b79c53d7a8ada8afd96
Franklin Kong <franklinkong981@gmail.com>
2025-03-21 12:38:00 -0700

    Implemented merge helper function in mergesort

--- merge.js
+++ merge.js
@@ -1,4 +1,30 @@
-function merge() {}
+function merge(arr1, arr2) {
+  let p1 = 0;
+  let p2 = 0;
+  let merrged_arr = [];
+
+  while (p1 < arr1.length && p2 < arr2.length) {
+    if (arr1[p1] < arr2[p2]) {
+      merrged_arr.push(arr1[p1]);
+      p1++;
+    } else {
+      merrged_arr.push(arr2[p2]);
+      p2++;
+    }
+  }
+
+  while (p1 < arr1.length) {
+    merrged_arr.push(arr1[p1]);
+    p1++;
+  }
+
+  while (p2 < arr2.length) {
+    merrged_arr.push(arr2[p2]);
+    p2++;
+  }
+
+  return merrged_arr;
+}
 
 function mergeSort() {}
 
