47db154c602e97a60ad96161d83f3a3b3db58266
Franklin Kong <franklinkong981@gmail.com>
2025-03-22 12:44:33 -0700

    Successfully implemented getDigit

--- radix.js
+++ radix.js
@@ -1,3 +1,21 @@
-function radixSort() {}
+function getDigit(num, digitIndex) {
+  //digitIndex of 0 = gets ones digit, digitIndex of 1 = get tens digit, etc.
+  for (let numTimesDivided = 0; numTimesDivided < digitIndex; numTimesDivided++) {
+    num = Math.floor(num / 10);
+  }
+  return num % 10;
+}
 
-module.exports = radixSort;
+function digitCount() {
+
+}
+
+function mostDigits() {
+  
+}
+
+function radixSort() {
+
+}
+
+module.exports = { getDigit, digitCount, mostDigits, radixSort };
